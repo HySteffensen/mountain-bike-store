50972d368f524493a596be2d311a024c575c4fdb
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-26 17:19:07 -0700

    added jshint to jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -2,8 +2,15 @@
 	"use strict";
 
 	desc("Default build");
-	task("default", function() {
+	task("default", [ "lint" ], function() {
 		console.log("\n\nBUILD OK");
 	});
+
+  desc("Lint JavaScript code");
+  task("lint", function() {
+    console.log("Linting JavaScript: .");
+
+    jake.exec("node_modules/jshint/bin/jshint Jakefile.js", { interactive: true }, complete);
+  }, { async: true });
 
 }());
