c8f33beeeda9d809fde170ae26f57503d21407ee
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-26 22:05:57 -0700

    factored out example test into multiple files

--- src/test.js
+++ src/test.js
@@ -1,17 +1,13 @@
 (function() {
   "use strict";
 
-
+  var addition = require("./addition.js");
 
   describe("addition", function() {
     it("adds", function() {
-      assertEqual(add(3, 4), 7);
+      assertEqual(addition.add(3, 4), 7);
     });
   });
-
-  function add(a, b) {
-    return a + b;
-  }
 
   function assertEqual(actual, expected) {
     if (actual !== expected) throw new Error("expected " + expected + " but was " + actual);
