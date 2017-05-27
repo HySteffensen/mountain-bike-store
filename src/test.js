108cbe6d943997e28a4d3320a55a81696df22d16
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-26 20:43:09 -0700

    configured karma and got a sample test working

--- src/test.js
+++ src/test.js
@@ -1,16 +1,20 @@
 (function() {
   "use strict";
 
-  var assert = require('chai').assert;
+
 
   describe("addition", function() {
     it("adds", function() {
-      assert.equal(add(3, 4), 7);  
+      assertEqual(add(3, 4), 7);
     });
   });
 
   function add(a, b) {
     return a + b;
+  }
+
+  function assertEqual(actual, expected) {
+    if (actual !== expected) throw new Error("expected " + expected + " but was " + actual);
   }
 
 }());
