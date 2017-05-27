8f18026cad889d13169064381c6c147cfb24cd53
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-26 19:26:33 -0700

    set up jshint options

--- Jakefile.js
+++ Jakefile.js
@@ -1,5 +1,9 @@
+/* globals jake:false, desc:false, task:false, complete:false, fail:false */
+
 (function() {
 	"use strict";
+
+  var jshint = require("simplebuild-jshint");
 
 	desc("Default build");
 	task("default", [ "lint" ], function() {
@@ -8,9 +12,29 @@
 
   desc("Lint JavaScript code");
   task("lint", function() {
-    console.log("Linting JavaScript: .");
+    console.log("Linting JavaScript: ");
 
-    jake.exec("node_modules/jshint/bin/jshint Jakefile.js", { interactive: true }, complete);
+    jshint.checkFiles({
+      files: "Jakefile.js",
+      options: {
+        bitwise: true,
+				eqeqeq: true,
+				forin: true,
+				freeze: true,
+				futurehostile: true,
+				latedef: "nofunc",
+				noarg: true,
+				nocomma: true,
+				nonbsp: true,
+				nonew: true,
+				strict: true,
+				undef: true,
+
+				node: true,
+				browser: true
+      },
+      globals: {}
+    }, complete, fail);
   }, { async: true });
 
 }());
